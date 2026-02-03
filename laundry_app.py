d0ce24d06b3e8e3e6d71c56151878de0de5923c8
Mostafa Mohamed <mostafa@gmail.com>
2026-02-03 04:27:07 +0200

    Fix date comparison error

--- laundry_app.py
+++ laundry_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from datetime import date
+from datetime import datetime, date as dt_date
+
 from datetime import datetime
 
 st.set_page_config(page_title="مغسلة المتحدة للسجاد")
@@ -37,7 +38,8 @@ if tab == "الحجز":
         submit = st.form_submit_button("احجز")
 
         if submit:
-         cutoff_date = date(datetime.now().year, 3, 20)
+    cutoff_date = dt_date(datetime.now().year, 3, 20)
+
 
             if date > cutoff_date:
                 message = "❌ لا يمكن الحجز بعد يوم 20/3"
